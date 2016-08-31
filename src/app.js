8f2b725c2e465678e1c575e834893e000d0e9954
owain-stratton <owain@geoshepherds.com>
2016-08-31 11:03:26 +0200

    server setup using nodejs and express for REST API

---
+++ src/app.js
@@ -0,0 +1,39 @@
+// Develop a REST API Server to gather and process visitor data. Provide GoogleMaps viewer frontend for previewing last 100 unique visits
+'use strict';
+var express     = require('express'),
+    routes      = require('./routes/router.js'),
+    morgan      = require('morgan'),
+    parser      = require('body-parser').json;
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(parser());
+
+app.use('/', routes);
+
+
+
+
+app.use(function(req, res, next) {
+  var err = new Error('The page you were looking for could not be found please try another');
+  err.status = 404;
+  next(err);
+});
+
+// Error event handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+
+var port = process.env.PORT || 9090;
+
+app.listen(port, function() {
+  console.log('The server is running on port 9090. Code on!!!');
+});
---
+++ src/routes/router.js
@@ -0,0 +1,28 @@
+// Input data from remote tracking nodes at '/api/track'
+  // if dupliate visit only send visit with strongest signal (rssi)
+  // Only archive data from tracker and internet_provider
+  // Locally store data from mobile_station and drone for map rendering
+
+// Processed data from '/api/track' to archive server at '/api/archive'
+ // Only accepts 'POST'
+ // Mandatory 'X-Auth-Token: Tracker1234' header
+ // Response 'status: ok'
+ // Response JSON
+ // Response 200 code on success
+ // Response 403 code on forbidden (invalid token)
+ // Response 400 code on malformed input data (all fields required)
+
+// Frontend viewer at '/viewer'
+ // Serve Google maps view with last 100 unique visits
+ // Visits grouped on markers
+ // Truncate position to 4 decimal places (not rounding)
+'use strict';
+var express     = require('express'),
+    router      = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.json('hello world');
+});
+
+module.exports = router;
